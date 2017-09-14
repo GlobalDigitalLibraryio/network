7d258d80717434cfe01cce8527fb3652c0483673
Kenneth Stigen <kes@knowit.no>
2017-09-14 10:16:17 +0200

    DNS is not switched

--- src/main/scala/io/digitallibrary/network/Domains.scala
+++ src/main/scala/io/digitallibrary/network/Domains.scala
@@ -11,6 +11,8 @@ object Domains {
 
   def get(environment: String): String = Map(
     "local" -> "http://local.digitallibrary.io",
+    "test" -> "http://test-proxy-1865761686.eu-central-1.elb.amazonaws.com",
+    "staging" -> "http://staging-proxy-95967625.eu-central-1.elb.amazonaws.com",
     "prod" -> "http://api.digitallibrary.io"
   ).getOrElse(environment, s"http://$environment.api.digitallibrary.io")
 

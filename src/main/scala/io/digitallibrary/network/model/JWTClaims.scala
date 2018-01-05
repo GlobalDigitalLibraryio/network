3a7a1e7ee0d6a6fb7b2e9033f34e3f53baf26e89
Kenneth Stigen <kes@knowit.no>
2018-01-05 13:44:30 +0100

    #161: Changed to accept any value in json GlobalDigitalLibraryio/issues#161

--- src/main/scala/io/digitallibrary/network/model/JWTClaims.scala
+++ src/main/scala/io/digitallibrary/network/model/JWTClaims.scala
@@ -12,7 +12,6 @@ import org.json4s.native.JsonMethods.parse
 
 case class JWTClaims(iss: Option[String],
                      sub: Option[String],
-                     aud: Option[String],
                      iat: Option[Long],
                      exp: Option[Long],
                      azp: Option[String],
@@ -28,16 +27,15 @@ object JWTClaims {
   val user_name_key = "https://digitallibrary.io/user_name"
 
   def apply(claims: JwtClaimsSet): JWTClaims = {
-    val content = parse(claims.asJsonString).extract[Map[String, String]]
-    JWTClaims(content.get("iss"),
-      content.get("sub"),
-      content.get("aud"),
+    val content = parse(claims.asJsonString).extract[Map[String, Any]]
+    JWTClaims(content.get("iss").asInstanceOf[Option[String]],
+      content.get("sub").asInstanceOf[Option[String]],
       content.get("iat").asInstanceOf[Option[Long]],
       content.get("exp").asInstanceOf[Option[Long]],
-      content.get("azp"),
-      content.get("gty"),
-      content.get("scope").map(_.split(' ').toList).getOrElse(List.empty),
-      content.get(gdl_id_key),
-      content.get(user_name_key))
+      content.get("azp").asInstanceOf[Option[String]],
+      content.get("gty").asInstanceOf[Option[String]],
+      content.get("scope").asInstanceOf[Option[String]].map(_.split(' ').toList).getOrElse(List.empty),
+      content.get(gdl_id_key).asInstanceOf[Option[String]],
+      content.get(user_name_key).asInstanceOf[Option[String]])
   }
 }
